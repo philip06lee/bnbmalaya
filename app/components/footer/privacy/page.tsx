fefb26251caba66de69bcc3b67cd2c55e0b37ca2
philip06lee <philip06lee@gmail.com>
2023-04-22 17:38:06 +0800

    first commit

--- app/components/footer/privacy/page.tsx
+++ app/components/footer/privacy/page.tsx
@@ -8,7 +8,7 @@ const PrivacyPage = () => {
         <div className="m-12 w-[80%] mx-auto bg-primary-purple-light flex flex-col justify-center items-center p-8 md:p-16 lg:p-24 xl:p-32 gap-12">
           <h2 className="text-lg font-bold">PRIVACY POLICY</h2>
           <p className="text-primary-white">  
-          We take protecting your data seriously. Our robust infrastructure provides data and asset protection you can trust. For information gathered from visitors, the log files are stored on our web server saving details. The visitor's email and username will be stored on the server. The information is used to enhance the visitor's experience when using the website. E-mail addresses and other information collected by Bnbmalaya.com will not be sold, rented or leased to 3rd parties. E-mails may be sent to inform you of news of our services or offers by us or our affiliates.</p>
+          We take protecting your data seriously. Our robust infrastructure provides data and asset protection you can trust. For information gathered from visitors, the log files are stored on our web server saving details. The visitor&apos;s email and username will be stored on the server. The information is used to enhance the visitor&apos;s experience when using the website. E-mail addresses and other information collected by Bnbmalaya.com will not be sold, rented or leased to 3rd parties. E-mails may be sent to inform you of news of our services or offers by us or our affiliates.</p>
         </div>
       </div> 
     </Container> 
--- app/components/footer/terms/page.tsx
+++ app/components/footer/terms/page.tsx
@@ -8,7 +8,7 @@ const TermsPage = () => {
         <div className="m-12 w-[80%] mx-auto bg-primary-purple-light flex flex-col justify-center items-center p-8 md:p-16 lg:p-24 xl:p-32 gap-12">
           <h1 className="text-xl font-bold">TERMS</h1>
           <p>1. Once you book, we will contact the host to check their availability. Once the host confirms they are available for your dates, we will then inform you accordingly.</p> 
-          <p>2. The host will then receive payment from you directly on your arrival. The host can then pay 5% of the booking total to Bnbmalaya. It's that simple!</p>
+          <p>2. The host will then receive payment from you directly on your arrival. The host can then pay 5% of the booking total to Bnbmalaya. It&apos;s that simple!</p>
           <p></p>
           
         </div>
